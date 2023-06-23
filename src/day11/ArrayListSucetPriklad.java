6480f77cb81c255501996dc1d3c3c0c3e802f280
Ján Kavan <jan.kavan2@gmail.com>
2023-06-23 22:09:23 +0200

    Update úloha č.1 z 22.06.2023 riešenie nepresnosti double matematiky.

--- src/day11/ArrayListSucetPriklad.java
+++ src/day11/ArrayListSucetPriklad.java
@@ -1,64 +1,96 @@
 package day11;
 
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Locale;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
+/**
+ * Riešenie príkladu Úloha č.1 z 22.06.2023 "Sčítanie vstupných hodnôt cez ArrayList". Trieda používa pre
+ * ukladanie desatinných čísiel typ Long pre riešenie problému presnosti matematiky čísiel typu double.
+ * Desatinné čísla sa ukladajú v dynamickom poli ako celé po zaokrúhlení na {@value DESATINNE_MIESTA} desatinné miesta
+ * a vynásobení 10^{@value DESATINNE_MIESTA}.
+ */
 public class ArrayListSucetPriklad {
 
-    /**
-     * Metóda pre výpis čísiel v dynamickom poli na konzolu.
-     *
-     * @param cislaPreVypis
-     */
-    private static void vypisCisiel(ArrayList<Double> cislaPreVypis) {
-        for (int i = 0; i < cislaPreVypis.size(); i++) {
-            System.out.println(i + " : " + cislaPreVypis.get(i));
-        }
-    }
+    final private static int DESATINNE_MIESTA = 3;
+    final private static int NASOBOK = (int) Math.pow(10, DESATINNE_MIESTA);
 
     /**
-     * Vráti súčet poľa.
+     * Vstup čísla z konzoly. Pri výnimke sa opakuje vstup s výzvou na zadanie správnej hodnoty.
      *
-     * @param cislaPreSucet
-     * @return double
+     * @param vyzva
+     * @return Desatinné číslo zaokrúhlené na {@value DESATINNE_MIESTA} desatinné miesta ako celé číslo vynásobené 10^{@value DESATINNE_MIESTA}.
      */
-    private static double sucetCisiel(ArrayList<Double> cislaPreSucet) {
-        double sucet = 0;
-        for (int i = 0; i < cislaPreSucet.size(); i++) {
-            sucet += cislaPreSucet.get(i);
-        }
-        return sucet;
-    }
-
-    public static void main(String[] args) throws IOException {
-        ArrayList<Double> ciselnePole = new ArrayList<>();
-        boolean chybaVstupu = false;
-        double novaHodnotaPrePole = 0;
-
-        System.out.println("Vstup hodnôt pre súčet je možný po výzve \"Zadaj hodnotu:\"");
-        System.out.println("Zadávanie hodnôt je možné ukončiť po zadaní záporného čísla.");
-
+    private static long vstupCisla(String vyzva) {
+        boolean chybaVstupu;
+        double vstup = 0;
         do {
             Scanner inputScan = new Scanner(System.in);
             chybaVstupu = false;
+            System.out.print(vyzva);
             try {
-                for (; ; ) {
-                    System.out.print("Zadaj hodnotu: ");
-                    novaHodnotaPrePole = inputScan.nextDouble();
-                    if (novaHodnotaPrePole < 0) break;
-                    ciselnePole.add(novaHodnotaPrePole);
-                }
-            } catch (java.util.InputMismatchException e) {
-                System.out.print("\n\nAko vstup je možné použiť len desatinné čísla!\n\n");
+                vstup = inputScan.nextDouble();
+            } catch (InputMismatchException e) {
+                System.out.println("\nAko vstup je možné použiť len desatinné čísla!\n");
                 chybaVstupu = true;
             }
         } while (chybaVstupu);
+        return Math.round(vstup * NASOBOK);
+    }
 
-        if (ciselnePole.size() > 0) {
-            vypisCisiel(ciselnePole);
-            System.out.println("\nSúčet zadaného poľa je:" + sucetCisiel(ciselnePole));
+
+    /**
+     * Vypíše celé čísla uložené v dynamickom poli ako destinné čísla zaokrúhlené na {@value DESATINNE_MIESTA} desatinné miesta.
+     *
+     * @param cislaPreVypis
+     */
+    private static void vypisCisiel(ArrayList<Long> cislaPreVypis) {
+        for (int i = 0; i < cislaPreVypis.size(); i++) {
+            double cisloPreVypis = (double) cislaPreVypis.get(i) / NASOBOK;
+            System.out.printf("%d : %." + DESATINNE_MIESTA + "f\n", i, cisloPreVypis);
         }
+    }
+
+    /**
+     * Vypočíta súčet poľa.
+     *
+     * @param cislaPreSucet
+     * @return Súčet ako double na {@value DESATINNE_MIESTA} desatinné miesta.
+     */
+    private static double sucetCisiel(ArrayList<Long> cislaPreSucet) {
+        long sucet = 0;
+        for (int i = 0; i < cislaPreSucet.size(); i++) {
+            sucet += cislaPreSucet.get(i);
+        }
+        return (double) sucet / NASOBOK;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Long> ciselnePole = new ArrayList<>();
+        long novyVstup = 0;
+        String vyzvaPreZadanieVstupu = "Zadaj číslo: ";
+
+        System.out.println("Vstup hodnôt pre súčet je možný po výzve \"" + vyzvaPreZadanieVstupu + "\"\n"
+                + "Zadané hodnoty budú zaokrúhlené na " + DESATINNE_MIESTA + " desatinné miesta!\n"
+                + "Program sa skončí po zadaní záporného čísla.\n");
+
+        //Cyklus pre vytvorenie poľa hodnôt zo vstupu zadaného používateľom.
+        for (; ; ) {
+            novyVstup = vstupCisla(vyzvaPreZadanieVstupu);
+            if (novyVstup < 0) break;
+            ciselnePole.add(novyVstup);
+        }
+
+        System.out.println();
+
+        //Ošetrenie situácie, keď používateľ ukončil program zadaním záporného čísla ako prvej hodnoty.
+        if (ciselnePole.size() > 0) {
+            System.out.println("Boli zadané nasledujúce čísla:");
+            vypisCisiel(ciselnePole);
+            System.out.printf("\nSúčet zadaného poľa je: %." + DESATINNE_MIESTA + "f\n", sucetCisiel(ciselnePole));
+        } else {
+            System.out.println("Neboli zadané žiadne hodnoty!");
+        }
+
     }
 }
